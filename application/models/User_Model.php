77033af31a602959a6163dbd5a51167c1d914a10
SEuDor <SEuDor@users.noreply.github.com>
2015-08-26 19:20:09 +0300

    Update User_Model.php

--- application/models/User_Model.php
+++ application/models/User_Model.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Женя
- * Date: 08.05.2015
- * Time: 16:33
- */
-
 class User_Model
 {
     public static function addUser($name, $surname, $gender, $skype, $login, $pas)
