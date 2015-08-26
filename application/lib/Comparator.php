4822a8433b5646229cf20a8aed917a443c260d3d
SEuDor <SEuDor@users.noreply.github.com>
2015-08-26 19:16:04 +0300

    Update Comparator.php

--- application/lib/Comparator.php
+++ application/lib/Comparator.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Женя
- * Date: 07.06.2015
- * Time: 2:42
- */
-
 class Comparator
 {
     public static function compare($val1, $val2, callable $compareFunction)
